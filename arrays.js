4f28d1c096033a87871cf13bb5205ec6b2aadc42
Albert Chun <albert.s.chun@gmail.com>
2018-08-01 18:43:46 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -19,8 +19,7 @@ function destructivelyAddElementToEndOfArray(array, element) {
 }
 
 function accessElementInArray(array, index) {
- let element = accessElementInArray[index];
- return element[""];
+ return array[index]
 }
 
 function destructivelyRemoveElementFromBeginningofArray(array) {
