0b84980a6f1ec857d37d14439e76c1c8366dea37
Albert Chun <albert.s.chun@gmail.com>
2018-08-01 18:58:38 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -22,7 +22,7 @@ function accessElementInArray(array, index) {
  return array[index];
 }
 
-function destructivelyRemoveElementFromBeginningofArray(array) {
+function destructivelyRemoveElementFromBeginningOfArray(array) {
   return array.shift();
 } 
 
