49b5524c619c43203657c7705ce348c25f724dc0
Albert Chun <albert.s.chun@gmail.com>
2018-08-01 17:06:34 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,10 +1,5 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
 function addElementToBeginningOfArray(array, element) {
-
 }
 
-function destructivelyAddElementToBeginningOfArray(array, element) {
-// return should alter the original array, addElementToBeginningOfArray and not modify
-}
-  
