4126b595b57497bca8b2d6f0e9cb3106fbf3740e
Albert Chun <albert.s.chun@gmail.com>
2018-08-01 16:41:28 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,6 +1,6 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
-function addElementToBeginningOfArray([array], element) {
+function addElementToBeginningOfArray([array], 'foo') {
   return use ...array 
 }
   
